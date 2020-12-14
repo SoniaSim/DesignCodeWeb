f88342adba8aa0daa30afab097b1f4de1c5d28c8
Sonia <sonia@tymate.com>
2020-12-14 18:56:52 +0100

    feat: add herosection & config gatsby

---
+++ src/components/sections/HeroSection.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { Link } from "gatsby"
+import styled from "styled-components"
+
+function HeroSection() {
+  return (
+    <Wrapper>
+      <ContentWrapper>
+        <TextWrapper>
+          <Title>Design <br/> and code React apps</Title>
+          <Description>
+            Don’t skip design. Learn design and code, by building real apps with
+            React and Swift. Complete courses about the best tools.
+          </Description>
+        </TextWrapper>
+      </ContentWrapper>
+    </Wrapper>
+  )
+}
+
+export default HeroSection
+
+const Wrapper = styled.div`
+  background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+`
+
+const ContentWrapper = styled.div`
+  max-width: 1234px;
+  padding: 200px 30px;
+  margin: 0 auto;
+`
+
+const TextWrapper = styled.div`
+  max-width: 360px;
+  display: grid;
+  gap: 30px;
+`
+
+const Title = styled.h1`
+  font-weight: bold;
+  font-size: 60px;
+  color: white;
+`
+const Description = styled.p`
+  font-size: 17px;
+  line-height: 130%;
+`
